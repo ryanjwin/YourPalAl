a965a73022328c6b79ceb63a1c862ef9189430b6
Ryan <ryanjwin2020@gmail.com>
2022-02-10 10:17:41 -0700

    changed the way roastme is read

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ import random
 import discord
 from dotenv import load_dotenv
 
+# create list of possibleRoasts from local text file
+# name text file roast.txt
+with open('roast.txt') as roast:
+    possibleRoasts = roast.readlines()
 
 # load bot token from env file
 # token can be created by creating an application on discord here: https://discord.com/login?redirect_to=%2Fdevelopers%2Fapplications
@@ -20,7 +24,7 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!') # prints in console if bot connection was successful
 
 @client.event
-async def on_message(message):
+async def on_message(message, possibleRoasts):
     user_typing = message.author
     if message.author == client.user:
         return # ensures that message is not from the bot
@@ -33,14 +37,13 @@ async def on_message(message):
         msg = await client.wait_for('message', check=lambda message: message.author == user_typing) # make sure that second message is the same author
         await message.channel.send("jajajaja")
 
-    # what a save
+    # what a save (rocket league reference)
     if message.content.lower() == "what a save":
         for i in range(3):
             await message.channel.send("What a save!")
         await message.channel.send("Chat disabled for 3 seconds")
     
     # roast me
-    possibleRoasts = ["What did the 2 say to the 10?\n roast me hahaha", "I'd slap you, \nbut that would be animal abuse.", "I am not saying that I hate you,\n I'm just saying if you got hit by a bus, I would be driving that bus.", "https://www.menshealth.com/uk/style/a29772406/receding-hairline/", "When I see your face, there is not a thing I would change...\n except the direction I'm walking in", "A little advice homie\nhttps://www.wikihow.com/Flirt"]
     if message.content.lower() == "roast me":
         idx = random.randint(0, len(possibleRoasts) - 1) # generates random integer for index in list
         await message.channel.send(possibleRoasts[idx])
